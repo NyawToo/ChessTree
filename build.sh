103b15dd91981bdaae92f2bf9d3ffbf44341c77a
NyawToo <julianandresperez904@hotmail.com>
2025-05-08 19:15:15 -0500

    RenderTry2

--- build.sh
+++ build.sh
@@ -11,8 +11,5 @@ python manage.py collectstatic --no-input
 # Aplicar migraciones
 python manage.py migrate
 
-# Asegurarse de que PORT tenga un valor predeterminado si no está definido
-export PORT=${PORT:-8000}
-
-# Iniciar Daphne
-exec daphne -b 0.0.0.0 -p $PORT Chess_Tree.asgi:application
+# # Iniciar Daphne
+# daphne -b 0.0.0.0 -p $PORT Chess_Tree.asgi:application
